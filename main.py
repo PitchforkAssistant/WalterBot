753a2fc949288d9f70e1d2dede60ce500f700126
PitchforkAssistant <pitchforkassistant@gmail.com>
2022-04-09 18:27:38 +0300

    Get rid of config.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import lightbulb
-
-from config import Config
+import configparser
 
 
 def start_bot() -> None:
-    config = Config()
+    config = configparser.ConfigParser()
+    config.read("config.ini")
     bot = lightbulb.BotApp(
-        token=config.token
+        token=config["DEFAULT"]["token"]
     )
     bot.load_extensions_from("./extensions", recursive=True)
     bot.run()
