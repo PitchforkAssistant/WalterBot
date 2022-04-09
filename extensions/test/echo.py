a9cda985f9e42178a13408db38164480ea108541
PitchforkAssistant <pitchforkassistant@gmail.com>
2022-04-09 20:08:50 +0300

    fix function name for echo

--- extensions/test/echo.py
+++ extensions/test/echo.py
@@ -5,12 +5,12 @@ from lightbulb import commands
 @lightbulb.option("text", "Any text.")
 @lightbulb.command("echo", "Repeats anything you write.")
 @lightbulb.implements(commands.SlashCommand)
-async def reload(ctx: lightbulb.context.Context) -> None:
+async def echo(ctx: lightbulb.context.Context) -> None:
     await ctx.respond(ctx.options.text)
 
 
 def load(bot: lightbulb.BotApp) -> None:
-    bot.command(reload)
+    bot.command(echo)
 
 
 def unload(bot: lightbulb.BotApp) -> None:
